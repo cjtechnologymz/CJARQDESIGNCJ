65724880ec7d38b58ee570bbb27f21909ff6f5f0
cjtechnologymz <142857102+cjtechnologymz@users.noreply.github.com>
2023-10-01 22:45:37 +0200

    Add files via upload

---
+++ pages/projectos/barbershop/vr1.js
@@ -0,0 +1,10 @@
+const panoramaImage = new PANOLENS.ImagePanorama("/pages/projectos/barbershop/f6.png");
+const imageContainer = document.querySelector(".image-container");
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+});
+
+viewer.add(panoramaImage);
---
+++ pages/projectos/edificio/vr3.js
@@ -0,0 +1,10 @@
+const panoramaImage = new PANOLENS.ImagePanorama("/pages/projectos/edificio/vr3.jpg");
+const imageContainer = document.querySelector(".image-container");
+
+const viewer = new PANOLENS.Viewer({
+  container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
+});
+
+viewer.add(panoramaImage);
